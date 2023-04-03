f3c383408fabfee912676dd4fa53bf41facf8c1f
zofiagrodecka <69384237+zofiagrodecka@users.noreply.github.com>
2023-04-03 13:45:48 +0200

    Fix multiplication of tiles'

--- Winter-Surf/Assets/Scripts/Ground/GroundSpawner.cs
+++ Winter-Surf/Assets/Scripts/Ground/GroundSpawner.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class GroundSpawner : MonoBehaviour {
     public GameObject groundTile;
     Vector3 nextSpawnPoint; 
-    int startingTilesNumber = 1;
-    // public static GroundSpawner instance;
+    int startingTilesNumber = 2;
 
 
     public void SpawnTile(){
@@ -14,15 +13,9 @@ public class GroundSpawner : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start(){
-        SpawnTile();
+        for(int i=0; i<startingTilesNumber; i++){
+            SpawnTile();
+        }
     }
-
-    // void Awake() {
-    //     if (instance == null) {
-    //         instance = this;
-    //     } else {
-    //         Destroy(gameObject);
-    //     }
-    // }
 
 }
--- Winter-Surf/Assets/Scripts/Ground/GroundTile.cs
+++ Winter-Surf/Assets/Scripts/Ground/GroundTile.cs
@@ -5,27 +5,21 @@ using System.Collections;
 public class GroundTile : MonoBehaviour {
     GroundSpawner groundSpawner;
     PlayerMove playerMove;
-    // public static GroundTile Instance;
-
-    // void Awake() {
-    //     if (Instance == null) {
-    //         Instance = this;
-    //     } else {
-    //         Destroy(gameObject);
-    //     }
-    // }
+    float spawnDelay;
 
      // Start is called before the first frame update
     void Start() {
         playerMove = GameObject.FindObjectOfType<PlayerMove>();
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
-        StartCoroutine(SpawnTileWithDelay(2));
+        spawnDelay = playerMove.getMoveSpeed()*2;
+        StartCoroutine(SpawnTileWithDelay(spawnDelay));
     }
 
     IEnumerator SpawnTileWithDelay(float delay) {
         while (true) {
             yield return new WaitForSeconds(delay);
             groundSpawner.SpawnTile();
+            Destroy(gameObject, delay);
         }
     }
 }
