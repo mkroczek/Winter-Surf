176e868539b13051c1565bf51d2153a9646ad836
Tomn0 <nal.tomek@gmail.com>
2023-04-11 16:17:37 +0200

    merged main to movement branch

---
+++ Winter-Surf/Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // transform.position = player.transform.
+        // transform.position = Vector3.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
