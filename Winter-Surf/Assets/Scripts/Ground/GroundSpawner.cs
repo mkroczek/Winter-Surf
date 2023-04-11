602a4f6967bfa6f0bcf194c497ca1912eb466bbf
Tomn0 <nal.tomek@gmail.com>
2023-04-11 20:12:57 +0200

    obstacle and collectable generation

--- Winter-Surf/Assets/Scripts/Ground/GroundSpawner.cs
+++ Winter-Surf/Assets/Scripts/Ground/GroundSpawner.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class GroundSpawner : MonoBehaviour {
     public GameObject groundTile;
     public GameObject pineObstacle;
+    public GameObject snowflakeCollectable;
     Vector3 nextSpawnPoint; 
     int startingTilesNumber = 2;
 
 
     public void SpawnTile(){
         GameObject obj = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
-        Instantiate(pineObstacle, obj.transform.position, Quaternion.identity);
         nextSpawnPoint = obj.transform.GetChild(1).transform.position;
+
+        Instantiate(pineObstacle, obj.transform.position, Quaternion.identity);
+
+        Vector3 snowflakePosition = new Vector3(obj.transform.position.x + 1, obj.transform.position.y, obj.transform.position.z);
+        Instantiate(snowflakeCollectable, snowflakePosition, Quaternion.identity);
     }
 
     // Start is called before the first frame update
