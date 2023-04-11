0980e80c85f47667537ebabaf9ce9a26eb60bd16
Tomn0 <nal.tomek@gmail.com>
2023-04-11 19:54:15 +0200

    base components for colissions and obstacle generation

--- Winter-Surf/Assets/GameController.cs
+++ Winter-Surf/Assets/GameController.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
-    public Transform player;
+public class GameController : MonoBehaviour
+{   
+    public GameObject terrain;
     // Start is called before the first frame update
     void Start()
     {
-        // transform.position = player.transform.
-        // transform.position = Vector3.
+        // GameObject newTerrain = Instantiate(terrain);
+        // newTerrain.transform.position = new Vector3(0, -1, 150);
     }
 
     // Update is called once per frame
--- Winter-Surf/Assets/Scripts/Ground/GroundSpawner.cs
+++ Winter-Surf/Assets/Scripts/Ground/GroundSpawner.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour {
     public GameObject groundTile;
+    public GameObject pineObstacle;
     Vector3 nextSpawnPoint; 
     int startingTilesNumber = 2;
 
 
     public void SpawnTile(){
         GameObject obj = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+        Instantiate(pineObstacle, obj.transform.position, Quaternion.identity);
         nextSpawnPoint = obj.transform.GetChild(1).transform.position;
     }
 
