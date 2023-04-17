6a7d74575b77e4c6acac09a5522edb1f18f4327c
Tomn0 <nal.tomek@gmail.com>
2023-04-17 17:23:34 +0200

    added animation of collision

---
+++ Winter-Surf/Assets/Scripts/Environmental/Collision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{   
+    public GameObject thePlayer;
+    public GameObject playerChild;
+
+    void OnTriggerEnter(Collider e) {
+
+        this.gameObject.GetComponent<MeshCollider>().enabled = false;
+        thePlayer.GetComponent<PlayerMove>().enabled = false;
+        // Animator playerAnimator = playerChild.GetComponent<Animator>();
+        playerChild.GetComponent<Animator>().Play("Stumble Backwards");
+        print("COLLISION");
+
+
+    }
+}
