2729c94e8cc684716b3b6da4a64e13cabdac68dd
zofiagrodecka <69384237+zofiagrodecka@users.noreply.github.com>
2023-05-14 16:03:16 +0200

    Add drawing probability for sections

--- Winter-Surf/Assets/Scripts/Environmental/GenerateLevel.cs
+++ Winter-Surf/Assets/Scripts/Environmental/GenerateLevel.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class GenerateLevel : MonoBehaviour
 {
     public GameObject[] section;
+    private List<int> weightedIndices = new List<int>();
     public int zPos = 16;
     public int increment = 10;
     public bool creatingSection = false;
     public int secNum;
     public GameObject pineObstacle;
     public GameObject snowflakeCollectable;
+
+    // Start is called before the first frame update
+    void Start(){
+        AssignWeights();
+    }
 
 
     // Update is called once per frame
@@ -33,7 +39,7 @@ public class GenerateLevel : MonoBehaviour
         }
 
         // section placement
-        secNum = Random.Range(0, 4);
+        int randomIndex = Random.Range(0, weightedIndices.Count);
         GameObject obj = Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
         zPos += increment;
         yield return new WaitForSeconds(2);
@@ -46,5 +52,29 @@ public class GenerateLevel : MonoBehaviour
         // snowflake placement
         Vector3 snowflakeVect = new Vector3(obj.transform.position.x + snowflakePosition, obj.transform.position.y + 0.5f, obj.transform.position.z);
         Instantiate(snowflakeCollectable, snowflakeVect, transform.rotation * Quaternion.Euler (90f, 0f, 0f));
+    }
+
+    void AssignWeights()
+    {
+        // Clear any previous weights
+        weightedIndices.Clear();
+
+        // Assign weights based on probabilities
+        for (int i = 0; i < section.Length; i++)
+        {
+            int weight;
+
+            if (i == 0){
+                weight = 30;
+            } else if (i < 3){
+                weight = 25;
+            } else{
+                weight = 10;
+            }
+
+            for (int j = 0; j < weight; j++) {
+                weightedIndices.Add(i);
+            }
+        }
     }
 }
